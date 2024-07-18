d02910c98834305224c874b229fa0ae2e3506cb2
ABINASH A. <allesabinash1@gmail.com>
2024-07-18 05:53:53 +0530

    card_page

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/global_variables.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
@@ -6,8 +7,41 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Text("Cart Page"),
-    ));
+        appBar: AppBar(
+          title: const Text(
+            "Cart",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        body: ListView.builder(
+            itemCount: cart.length,
+            itemBuilder: (context, index) {
+              final cartItem = cart[index];
+
+              return ListTile(
+                tileColor: Colors.grey,
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage(cartItem['imageUrl'] as String),
+                  radius: 30,
+                ),
+                title: Text(
+                  cartItem['title'].toString(),
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+                subtitle: Text(
+                  "Size: ${cartItem['size']}",
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+                trailing: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.delete,
+                      color: Colors.black,
+                    )),
+              );
+            }));
   }
 }
